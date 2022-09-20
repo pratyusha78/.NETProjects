3928b29d289e5856351982c4306a4e0cf90f0e74
Pratyusha <pratyushasingh78ee@gmail.com>
2022-09-20 15:30:52 +0530

    Initial commit

---
+++ Project1/Project1/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project1.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ Project1/Project1/Models/ProductsData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project1.Models
+{
+    public class ProductsData
+    {
+        public IEnumerable<Product> ProductsList
+        {
+            get
+            {
+                List<Product> p = new List<Product>()
+
+                { new Product{ProductId=1,Name="Reliance",Price=34642.34},new Product{ProductId=2,Name="Tata",Price=7024242.22} };
+
+               return p;
+            }
+ 
+        }
+    }
+}
